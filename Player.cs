73b3dcf9bc57bd1106a1e42eddb54f77c9e6c299
JudeH1 <90494861+JudeH1@users.noreply.github.com>
2024-02-15 15:24:58 -0500

    Add files via upload
    Here's a simple script that makes the player object roll around like a ball when you press the arrow keys (make sure the object has ridigbody2D).

---
+++ Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+private float horizontalInput;
+private float verticalInput;
+private float speed = 10.0f;
+private Rigidbody2D rb;
+// below is a diagram for direction
+// ACTUALLY NEVER MIND LETS NOT USE THAT
+// 1 2 3
+// 4 0 5
+// 6 7 8
+
+void Start ()
+{
+   rb = GetComponent<Rigidbody2D>();
+}
+
+void Update()
+{
+   horizontalInput = Input.GetAxis("Horizontal");
+   verticalInput = Input.GetAxis("Vertical");
+
+   
+}
+
+
+void FixedUpdate(){
+        rb.AddForce(new Vector3(horizontalInput, verticalInput, 0.0f) * speed);
+    }
+}
+
+
+
