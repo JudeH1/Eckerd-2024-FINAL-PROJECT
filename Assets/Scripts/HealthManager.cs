b2045c9685abf3d3eef593c499260ab0a0050126
JohnCetinkaya <jscetink@eckerd.edu>
2024-04-16 14:11:46 -0400

    Revert "Revert "Health And Attcks Working""
    This reverts commit a14d119b8cab0151dbf7d7fe2cd289d52e1f04ee.

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public float maxHealth = 3;
+
+    public float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth > 0)
+        {
+            //playerHurt update
+        }
+        else
+        {
+            //player Dead
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth += healAmount;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Heal(1);
+        }
+    }
+}
